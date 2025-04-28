3d16e3d44fc523a06f8172b3102b867a7edceccb
Lumana2002 <lumana.bajracharya@gmail.com>
2025-04-28 16:32:42 +0545

    Odd number sum

--- EvenSum.java
+++ EvenSum.java
@@ -10,7 +10,7 @@ public class EvenSum {
 
         int sum = 0;
         
-        for(int i =1; i<=num; i++){
+        for(int i = 1; i<=num; i++){
             if(i%2 == 0){
                 sum +=i;
                 // System.out.println(i);
