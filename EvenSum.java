db6444193d7352adf364f38ca1699518e8604027
Lumana2002 <lumana.bajracharya@gmail.com>
2025-04-28 15:58:48 +0545

    Sum of even num

---
+++ EvenSum.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class EvenSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number limit: ");
+        int num = scanner.nextInt();
+
+        int sum = 0;
+        
+        for(int i =1; i<=num; i++){
+            if(i%2 == 0){
+                sum +=i;
+                // System.out.println(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
